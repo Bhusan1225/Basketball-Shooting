8b5952f7b21561d690fe64f774a328e04e5cb291
bhusanSahoo <bhusan12sahoo@gmail.com>
2025-05-26 01:49:44 +0530

    basic throwing logic is done here

--- Assets/Script/Basketball/BasketballController.cs
+++ Assets/Script/Basketball/BasketballController.cs
@@ -9,6 +9,9 @@ public class BasketballController : MonoBehaviour
     [SerializeField] GameObject playerCamera;
 
     [SerializeField] float ballDistance = 2f;
+    [SerializeField] float ballThrowingForce = 5f;
+
+    [SerializeField] bool holdingBall = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +21,16 @@ public class BasketballController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        basketball .transform.position = playerCamera.transform.position + playerCamera.transform.forward * ballDistance;
+        if (holdingBall)
+        {
+            basketball.transform.position = playerCamera.transform.position + playerCamera.transform.forward * ballDistance;
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            holdingBall = false;
+            basketball.GetComponent <Rigidbody>().useGravity = true;
+            basketball.GetComponent<Rigidbody>().AddForce(playerCamera.transform.forward * ballThrowingForce);
+        }
+        
     }
 }
