d614035b2968161512db4edbf4899b3d6820473b
bhusanSahoo <bhusan12sahoo@gmail.com>
2025-05-26 13:50:17 +0530

    scoring UI and logic is also done

--- Assets/Script/Scoring.cs
+++ Assets/Script/Scoring.cs
@@ -1,14 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Scoring : MonoBehaviour
 {
+    [SerializeField] int score;
+    [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] int increament = 1;
+
+    private void Start()
+    {
+        score = 0;
+        scoreText.text = "Socre: " + score;
+    }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Basketball"))
+        if (other.gameObject.layer == 6)
         {
             Debug.Log("you scored 1 point.");
+            AddPoints(increament);
         }
     }
+
+    public void AddPoints(int points)
+    {
+        score += points;
+        scoreText.text = "Socre: " + score;
+    }
+
+    public void ReducePoints(int points)
+    {
+        score -= points;
+        scoreText.text = "Socre: " + score;
+    }
+
+
+
 }
